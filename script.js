e1cb32593eae92cfe5dba99ce59ed857969b42e5
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-08-11 15:07:35 +0200

    Update to play BO1

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-let choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     let randElement = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randElement)
+    return (randElement)
 }
+
+function playRound(playerSelection, computerSelection) {
+    console.log(`Computer Selection: ${computerSelection}`)
+    if (playerSelection === "rock") {
+        if (computerSelection === "Rock")
+            return "It's a tie!";
+        else if (computerSelection === "Paper")
+            return "You Lose! Paper beats Rock";
+        else if (computerSelection === "Scissors")
+            return "You win! Rock beats Scissors";
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "Rock")
+            return "You win! Paper beats Rock";
+        else if (computerSelection === "Paper")
+            return "It's a tie!";
+        else if (computerSelection === "Scissors")
+            return "You lose! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "Rock")
+            return "You lose! Rock beats Scissors";
+        else if (computerSelection === "Paper")
+            return "You win! Scissors beats Paper";
+        else if (computerSelection === "Scissors")
+            return "It's a tie!";
+    }
+}
+
+let playerSelection = prompt("Make your choice: ");
+console.log(`Player Selection: ${playerSelection}`)
+playerSelection = playerSelection.toLowerCase();
+
+console.log(playRound(playerSelection, getComputerChoice()));
